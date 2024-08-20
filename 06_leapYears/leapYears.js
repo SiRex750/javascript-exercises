6824003854193ce8c421dbce920b0aee7420d1f1
SiRex750 <119781835+SiRex750@users.noreply.github.com>
2024-08-20 14:03:34 +0530

    solved 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,20 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let c = 0;
+    if (year % 4 == 0) {
+        c++;
+    }
+    if (year % 100) {
+        c--;
+    }
+    if (year % 400) {
+        c++;
+    }
+    if (c == 1) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
