035a5d69c1cc6abde3d6e9ca73701b110bc55bf3
SiRex750 <119781835+SiRex750@users.noreply.github.com>
2024-08-20 15:34:44 +0530

    solved 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let newString = "", revString = "";
+    string = string.toLowerCase();
+    for (let i = 0; i < string.length; i++) {
+        if ((string[i] >= 'a' && string[i] <= 'z') || (string[i] >= '0' && string[i] <= '9')) {
+            newString += string[i];
+        }
+    }
+    for (let i = newString.length - 1; i > -1; i--) {
+        revString += newString[i];
+    }
+    if (newString === revString) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
