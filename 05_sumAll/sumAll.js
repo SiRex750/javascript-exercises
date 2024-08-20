c752fec25d0f17aaac12be2ea4c6f3f058a5c8b2
SiRex750 <119781835+SiRex750@users.noreply.github.com>
2024-08-20 13:51:37 +0530

    solved 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (a % 1 !== 0 || b % 1 !== 0) {
+        return "ERROR";
+    }
+    else if (typeof a !== "number" || typeof b !== "number") {
+        return "ERROR";
+    }
+    else if ( a < 0 || b < 0) {
+        return "ERROR";
+    }
+    else {
+        let sum = 0, x = 0, y = 0;
+        if (a > b) {
+            x = b;
+            y = a;
+        }
+        else {
+            x = a;
+            y = b;
+        }
+        for (let i = x; i <= y; i++) {
+            sum += i;
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
