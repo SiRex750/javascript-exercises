547d385d8a4776333e6ba15b5ac001aa53f400bb
SiRex750 <119781835+SiRex750@users.noreply.github.com>
2024-08-20 13:33:02 +0530

    Solved 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...removeArray) {
+    let newArray = [];
+    array.forEach((item) => {
+        if (!removeArray.includes(item)) {
+            newArray.push(item);
+        }
+    })
+    return newArray;
 };
 
 // Do not edit below this line
