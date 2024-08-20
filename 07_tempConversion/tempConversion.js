2c882e0d05cf3191aeb031d67e589228daeb1e3c
SiRex750 <119781835+SiRex750@users.noreply.github.com>
2024-08-20 14:19:33 +0530

    solved 07_tempConversions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fTemp) {
+  let cTemp = 0;
+  cTemp = (fTemp - 32) * 5 / 9;
+  return Number(cTemp.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cTemp) {
+  let fTemp = 0;
+  fTemp = cTemp * 9 / 5 + 32;
+  return Number(fTemp.toFixed(1));
 };
 
 // Do not edit below this line
